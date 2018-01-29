6f6dbdcbfa3e0feafa424d6b612730a05ce1ce67
Achyut Dev <achyut.dev@gmail.com>
2018-01-28 22:57:47 -0500

    First Commit

---
+++ luintel-school-api/src/main/java/hello/GreetingController.java
@@ -0,0 +1,29 @@
+package hello;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    private static final Logger logger = LogManager.getLogger(GreetingController.class);
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        logger.debug("Debugging log");
+        logger.info("Info log");
+        logger.warn("Hey, This is a warning!");
+        logger.error("Oops! We have an Error. OK");
+        logger.fatal("Damn! Fatal error. Please fix me.");
+        
+        return new Greeting(counter.incrementAndGet(),
+                            String.format(template, name));
+    }
+}
